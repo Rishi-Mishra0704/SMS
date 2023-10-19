b3521adf41c8ce05d5ea1d297c816841b267570a
Rishi-Mishra0704 <rishimishra0404@gmail.com>
2023-10-20 00:00:01 +0530

    update model and migrations

--- sms/apis/models.py
+++ sms/apis/models.py
@@ -1,9 +1,13 @@
 from django.db import models
-
 from core.models import CustomUser
 
 class Classroom(models.Model):
-    teacher = models.ForeignKey(CustomUser, on_delete=models.CASCADE, related_name='teaching_classes')
+    teacher = models.ForeignKey(
+        CustomUser,
+        on_delete=models.CASCADE,
+        related_name='teaching_classes',
+        limit_choices_to={'user_type': 'teacher'}
+    )
     students = models.ManyToManyField(CustomUser, related_name='enrolled_classes')
 
     def __str__(self):
