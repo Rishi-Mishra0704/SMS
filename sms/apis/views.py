01dd7a211c0d3c4394403e518f1f8de6ecf44175
Rishi-Mishra0704 <rishimishra0404@gmail.com>
2023-10-20 00:17:42 +0530

    add all crud operations

--- sms/apis/views.py
+++ sms/apis/views.py
@@ -12,3 +12,30 @@ def getClass(request):
     classrooms = Classroom.objects.all()
     serializer = ClassroomSerializer(classrooms, many=True)
     return Response(serializer.data)
+
+
+@api_view(['POST'])
+def postClass(request):
+    serializer = ClassroomSerializer(data=request.data)
+    if serializer.is_valid():
+        serializer.save()
+        return Response(serializer.data)
+    else:
+        return Response(serializer.errors)
+    
+@api_view(['GET','PUT','DELETE'])
+def classDetail(request, pk):
+    classroom = Classroom.objects.get(id=pk)
+    if request.method == 'GET':
+        serializer = ClassroomSerializer(classroom)
+        return Response(serializer.data)
+    elif request.method == 'PUT':
+        serializer = ClassroomSerializer(classroom, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        else:
+            return Response(serializer.errors)
+    elif request.method == 'DELETE':
+        classroom.delete()
+        return Response('Item deleted successfully')        
