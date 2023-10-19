b874e7e448ab37fed2a1905ae1cc46c6a2183392
Rishi-Mishra0704 <rishimishra0404@gmail.com>
2023-10-20 00:12:49 +0530

    add classroom endpoint

--- sms/apis/views.py
+++ sms/apis/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+
+from .serializer import ClassroomSerializer
+from .models import Classroom
 
 # Create your views here.
+
+@api_view(['GET'])
+def getClass(request):
+    classrooms = Classroom.objects.all()
+    serializer = ClassroomSerializer(classrooms, many=True)
+    return Response(serializer.data)
