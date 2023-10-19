c2f7d5ebfb0407cdbec1db4d1ed84f1dcbfa9616
Rishi-Mishra0704 <rishimishra0404@gmail.com>
2023-10-19 23:56:08 +0530

    add classroom model

--- sms/apis/models.py
+++ sms/apis/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+from core.models import CustomUser
+
+class Classroom(models.Model):
+    teacher = models.ForeignKey(CustomUser, on_delete=models.CASCADE, related_name='teaching_classes')
+    students = models.ManyToManyField(CustomUser, related_name='enrolled_classes')
+
+    def __str__(self):
+        return f"Classroom with Teacher: {self.teacher.username}"
