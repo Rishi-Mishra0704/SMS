cdbdd02f64e4bbbf60c8040687dcc1da543bf3e0
Rishi-Mishra0704 <rishimishra0404@gmail.com>
2023-10-21 23:00:01 +0530

    rails: add users api

--- s-m-s/app/controllers/users_controller.rb
+++ s-m-s/app/controllers/users_controller.rb
@@ -1,2 +1,17 @@
 class UsersController < ApplicationController
-end
+    def index
+      @users = User.all
+      render json: @users
+    end
+
+    def show
+        @user = User.find(params[:id])
+        render json: @user
+    end
+    
+    def destroy
+      @user = User.find(params[:id])
+      @user.destroy
+      head :no_content
+    end
+  end
